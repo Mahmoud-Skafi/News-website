c5dd8f90cd1aefb55e82e8d27f83b31381da3eb9
mahmoud skafi <197600@ppu.edu.ps>
2020-04-22 21:32:37 +0300

    dashboard style

---
+++ admin/navbar.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="../node_modules/jquery/dist/jquery.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./assets/css/dashboard.css">
+</head>
+
+<body>
+    <div class="sk-navbar-div">
+        <nav class="sk-navbar">
+            
+            <div class="sk-container">
+            <div class="sk-user-info">
+                    <img src="./assets/images/skafi.jpg" alt="">
+                    <p>mahmoudskafi</p>
+                </div>
+
+                <div class="sk-search-bar">
+                    <i class="fas fa-search"></i>
+                    <input type="text" placeholder="Search for post">
+                </div>
+
+                <div class="sk-nav-logo">
+                    <!-- <img src="./assets/images/unnamed.png" alt=""> -->
+                    <h1>News-website</h1>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="sk-sidebar-div">
+        <nav class="sk-sidebar">
+            <div class="sk-sidebar-continer">
+                <ul>
+                    <li>
+                        <span>
+                            <a href="">go
+                            </a>
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <a href="">
+
+                            </a>
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <a href="">
+
+                            </a>
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <a href="">
+
+                            </a>
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <a href="">
+
+                            </a>
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </div>
+    <div>
+        <h1>adadadad</h1>
+    </div>
+</body>
+
+</html>
